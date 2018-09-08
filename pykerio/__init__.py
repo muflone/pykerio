6e09a357a8252b7f31398c8ba71357d68e62e9ea
Muflone <webreg@vbsimple.net>
2018-09-09 00:47:51 +0200

    Renamed version number to __version__

--- pykerio/__init__.py
+++ pykerio/__init__.py
@@ -18,8 +18,8 @@
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 ##
 
+from .version import __version__
 from .pykerio import PyKerio
 from .pykerio_control import PyKerioControl
-from .version import version
 from .json_serializable import JSONSerializable
 from .rpc_response import RPCResponse
