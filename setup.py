7b1914a9393994a8512e33f39dfddbe29107c1e2
Muflone <webreg@vbsimple.net>
2018-09-09 14:42:14 +0200

    Added setup.py for distribution

---
+++ setup.py
@@ -0,0 +1,51 @@
+##
+#     Project: PyKerio
+# Description: API for Kerio products
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2018 Fabio Castelli
+#     License: GPL-2+
+#  This program is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by the Free
+#  Software Foundation; either version 2 of the License, or (at your option)
+#  any later version.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+#  more details.
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+##
+
+import os.path
+import setuptools
+
+import pykerio.constants
+
+
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                       'README.md'),
+          encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name=pykerio.constants.APP_NAME,
+    version=pykerio.constants.APP_VERSION,
+    author=pykerio.constants.APP_AUTHOR,
+    author_email=pykerio.constants.APP_AUTHOR_EMAIL,
+    description=pykerio.constants.APP_DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=pykerio.constants.APP_URL,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Planning ',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Networking :: Firewalls'
+    ],
+)
