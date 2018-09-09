a0b3bc44dbcc6d5427248bd562ada61c9fa2c54c
Muflone <webreg@vbsimple.net>
2018-09-09 14:04:46 +0200

    Changed author email address

--- pykerio/json_serializable.py
+++ pykerio/json_serializable.py
@@ -1,7 +1,7 @@
 ##
 #     Project: PyKerio
 # Description: API for Kerio products
-#      Author: Fabio Castelli (Muflone) <muflone@vbsimple.net>
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
 #   Copyright: 2018 Fabio Castelli
 #     License: GPL-2+
 #  This program is free software; you can redistribute it and/or modify it
