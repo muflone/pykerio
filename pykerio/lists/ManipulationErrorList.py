b3fe33d38db14ae05f55b1327d1a8ea63184c659
Muflone <webreg@vbsimple.net>
2018-10-07 01:09:00 +0200

    New list type ManipulationErrorList

--- pykerio/lists/ManipulationErrorList.py
+++ pykerio/lists/ManipulationErrorList.py
@@ -18,14 +18,11 @@
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 ##
 
-from .BaseList import BaseList
-from .ClientTimestampList import ClientTimestampList
-from .IntegerList import IntegerList
-from .KIdList import KIdList
-from .LocalizableMessageList import LocalizableMessageList
-from .ManipulationErrorList import ManipulationErrorList
-from .NamedMultiValueList import NamedMultiValueList
-from .NamedValueList import NamedValueList
-from .SortOrderList import SortOrderList
-from .StringList import StringList
-from .SubConditionList import SubConditionList
+from . import BaseList
+
+from ..structs.ManipulationError import ManipulationError
+
+
+class ManipulationErrorList(BaseList):
+    def __init__(self, *args, **kwargs):
+        BaseList.__init__(self, ManipulationError, *args, **kwargs)
