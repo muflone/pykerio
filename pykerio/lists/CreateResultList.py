0b7e95c4162c8ac4d81de69a3e3f368336beb372
Muflone <webreg@vbsimple.net>
2018-10-07 13:32:45 +0200

    New list type CreateResultList

--- pykerio/lists/CreateResultList.py
+++ pykerio/lists/CreateResultList.py
@@ -18,18 +18,14 @@
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 ##
 
-from .BaseList import BaseList
-from .AddResultList import AddResultList
-from .ClientTimestampList import ClientTimestampList
-from .CreateResultList import CreateResultList
-from .ErrorList import ErrorList
-from .IntegerList import IntegerList
-from .IpAddressList import IpAddressList
-from .KIdList import KIdList
-from .LocalizableMessageList import LocalizableMessageList
-from .ManipulationErrorList import ManipulationErrorList
-from .NamedMultiValueList import NamedMultiValueList
-from .NamedValueList import NamedValueList
-from .SortOrderList import SortOrderList
-from .StringList import StringList
-from .SubConditionList import SubConditionList
+from . import BaseList
+
+from ..structs.CreateResult import CreateResult
+
+
+class CreateResultList(BaseList):
+    """
+    List of Sorting Orders
+    """
+    def __init__(self, *args, **kwargs):
+        BaseList.__init__(self, CreateResult, *args, **kwargs)
--- tests/lists/test_createresultlist.py
+++ tests/lists/test_createresultlist.py
@@ -18,18 +18,28 @@
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 ##
 
-from .BaseList import BaseList
-from .AddResultList import AddResultList
-from .ClientTimestampList import ClientTimestampList
-from .CreateResultList import CreateResultList
-from .ErrorList import ErrorList
-from .IntegerList import IntegerList
-from .IpAddressList import IpAddressList
-from .KIdList import KIdList
-from .LocalizableMessageList import LocalizableMessageList
-from .ManipulationErrorList import ManipulationErrorList
-from .NamedMultiValueList import NamedMultiValueList
-from .NamedValueList import NamedValueList
-from .SortOrderList import SortOrderList
-from .StringList import StringList
-from .SubConditionList import SubConditionList
+import unittest
+
+import pykerio.lists
+
+
+class TestCase_CreateResultList(unittest.TestCase):
+    def test_01_CreateResultList(self):
+        """
+        Test CreateResultList
+        """
+        testlist = pykerio.lists.CreateResultList()
+        self.assertEquals(len(testlist), 0)
+
+        index = 12
+        kid = pykerio.shared.KId('user1')
+        teststruct = pykerio.structs.CreateResult({
+            'inputIndex': index,
+            'id': kid})
+
+        testlist.append(teststruct)
+        self.assertEquals(len(testlist), 1)
+        self.assertEquals(testlist[-1], teststruct)
+
+        testlist.clear()
+        self.assertEquals(len(testlist), 0)
