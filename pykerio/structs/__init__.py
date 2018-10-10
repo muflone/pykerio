cf20d9953409605994b8605464e358b7b1072dd5
Muflone <webreg@vbsimple.net>
2018-10-10 16:55:43 +0200

    New struct CertificateDn

--- pykerio/structs/__init__.py
+++ pykerio/structs/__init__.py
@@ -23,6 +23,7 @@ from .AddResult import AddResult
 from .ApiApplication import ApiApplication
 from .ApiException import ApiException
 from .ByteValueWithUnits import ByteValueWithUnits
+from .CertificateDn import CertificateDn
 from .ClientTimestamp import ClientTimestamp
 from .Collision import Collision
 from .ConnectivityConfig import ConnectivityConfig
---
+++ tests/structs/test_certificatedn.py
@@ -0,0 +1,47 @@
+##
+#     Project: PyKerio
+# Description: API for Kerio products
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2018 Fabio Castelli
+#     License: GPL-2+
+#  This program is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by the Free
+#  Software Foundation; either version 2 of the License, or (at your option)
+#  any later version.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+#  more details.
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+##
+
+import unittest
+
+import pykerio.lists
+import pykerio.structs
+
+
+class TestCase_CertificateDn(unittest.TestCase):
+    def test_01_CertificateDn(self):
+        """
+        Test CertificateDn
+        """
+        name = 'certificate1'
+        certificate = pykerio.structs.IdReference({
+            'id': pykerio.types.KId(name),
+            'name': name,
+            'invalid': False})
+        teststruct = pykerio.structs.CertificateDn({
+            'certificate': certificate,
+            'value': name})
+        self.assertEquals(len(teststruct.keys()), 2)
+        self.assertEquals(len(teststruct.values()), 2)
+
+        self.assertEquals(teststruct['certificate'], certificate)
+        self.assertEquals(teststruct['value'], name)
+
+        teststruct.clear()
+        self.assertEquals(len(teststruct.keys()), 0)
