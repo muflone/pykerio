285e7588f80b7006a138989ebf414d51a0cd6b1c
Muflone <webreg@vbsimple.net>
2018-10-21 23:00:25 +0200

    New list type RestrictionList

--- pykerio/lists/__init__.py
+++ pykerio/lists/__init__.py
@@ -45,6 +45,7 @@ from .NotificationTypeList import NotificationTypeList
 from .OptionalIpAddressList import OptionalIpAddressList
 from .OptionalStringList import OptionalStringList
 from .PortConfigList import PortConfigList
+from .RestrictionList import RestrictionList
 from .RestrictionTupleList import RestrictionTupleList
 from .SortOrderList import SortOrderList
 from .SpeedDuplexMayNotWorkList import SpeedDuplexMayNotWorkList
