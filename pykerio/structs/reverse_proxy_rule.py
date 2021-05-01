ca59c98239732fe7aaa9e077a3286f7fe7b6f081
Kostiantyn Kostiuk <kostyanf14@live.com>
2021-05-01 19:07:05 +0300

    Add new struct ReverseProxyRule
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

---
+++ pykerio/structs/reverse_proxy_rule.py
@@ -0,0 +1,43 @@
+##
+#     Project: PyKerio
+# Description: API for Kerio products
+#      Author: Kostiantyn Kostiuk <kostyanf14@live.com>
+#   Copyright: 2018-2023 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+from pykerio.structs.base_struct import BaseStruct
+
+from pykerio.enums.https_server_mode import HttpsServerMode
+
+from pykerio.structs.id_reference import IdReference
+
+from pykerio.types.kid import KId
+
+
+class ReverseProxyRule(BaseStruct):
+    def __init__(self, data: dict):
+        BaseStruct.__init__(self,
+                            types={'serverHostname': str,
+                                   'serverHttp': bool,
+                                   'httpsMode': HttpsServerMode,
+                                   'customCertificate': IdReference,
+                                   'targetServer': str,
+                                   'targetHttps': bool,
+                                   'antivirus': bool,
+                                   'enabled': bool,
+                                   'description': str,
+                                   'id': KId},
+                            data=data)
---
+++ tests/structs/test_struct_reverse_proxy_rule.py
@@ -0,0 +1,78 @@
+##
+#     Project: PyKerio
+# Description: API for Kerio products
+#      Author: Kostiantyn Kostiuk <kostyanf14@live.com>
+#   Copyright: 2018-2023 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+import unittest
+
+import pykerio
+
+
+class TestCase_ReverseProxyRule(unittest.TestCase):
+    def test_01_ReverseProxyRule(self):
+        """
+        Test ReverseProxyRule
+        """
+
+        domain = 'domain.com'
+        serverHttp = False
+        httpsMode = pykerio.enums.HttpsServerMode.HttpsServerModeCustomCertificate
+        customCertificateId = '66315de7-48b2-b844-81fa-c08f5e0f6144'
+        customCertificateInvalid = False
+        targetServer = '10.0.0.10'
+        targetHttps = True
+        antivirus = False
+        enabled = True
+        description = 'Reverse proxy HTTPS rule for domain.com'
+        rule_id = '148'
+
+        customCertificate = pykerio.structs.IdReference({
+            'id': customCertificateId,
+            'name': domain,
+            'invalid': customCertificateInvalid
+        })
+
+        teststruct = pykerio.structs.ReverseProxyRule(
+            {
+                'serverHostname': domain,
+                'serverHttp': serverHttp,
+                'httpsMode': httpsMode,
+                'customCertificate': customCertificate,
+                'targetServer': targetServer,
+                'targetHttps': targetHttps,
+                'antivirus': antivirus,
+                'enabled': enabled,
+                'description': description,
+                'id': rule_id
+            })
+
+        self.assertEqual(len(teststruct.keys()), 10)
+        self.assertEqual(len(teststruct.values()), 10)
+
+        self.assertEqual(teststruct['serverHostname'], domain)
+        self.assertEqual(teststruct['serverHttp'], serverHttp)
+        self.assertEqual(teststruct['httpsMode'], httpsMode)
+        self.assertEqual(teststruct['customCertificate'], customCertificate)
+        self.assertEqual(teststruct['targetServer'], targetServer)
+        self.assertEqual(teststruct['targetHttps'], targetHttps)
+        self.assertEqual(teststruct['antivirus'], antivirus)
+        self.assertEqual(teststruct['description'], description)
+        self.assertEqual(teststruct['id'], rule_id)
+
+        teststruct.clear()
+        self.assertEqual(len(teststruct.keys()), 0)
